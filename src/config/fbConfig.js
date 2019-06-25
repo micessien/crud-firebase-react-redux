8e7ac691c08669b5727f598c58f105ab896fec14
Micessien <micaeldie@gmail.com>
2019-06-25 19:37:40 +0000

    Using thunk & Creating/Config a firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD4Ge4JmFTAEtVKC9pDtBAPvjSdWGP3AvU",
+  authDomain: "marioplan-8fab9.firebaseapp.com",
+  databaseURL: "https://marioplan-8fab9.firebaseio.com",
+  projectId: "marioplan-8fab9",
+  storageBucket: "",
+  messagingSenderId: "775830023493",
+  appId: "1:775830023493:web:ca68a9e3f33ea388"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
