06e45134ddac0774ebcea39c0eb0ed8ade372f99
Micessien <micaeldie@gmail.com>
2019-06-26 21:03:30 +0000

    Deployed

---
+++ dist/precache-manifest.4390cacd0f44d268741930c50391ff02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f6c5d25f16a15b4a574f41d5ee96b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19dbbef434ab096e3de",
+    "url": "/static/css/main.24466092.chunk.css"
+  },
+  {
+    "revision": "e441d3d0e5d37f31b45b",
+    "url": "/static/js/2.7bf55930.chunk.js"
+  },
+  {
+    "revision": "e19dbbef434ab096e3de",
+    "url": "/static/js/main.7feb11d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
